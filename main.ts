eb272f9691300a653fc9b1b281953e211c4e54bd
muddygrass <118327888+muddygrass@users.noreply.github.com>
2022-11-16 14:03:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.onReceivedValue(function (name, value) {
+    serial.writeValue(name, value)
+})
+radio.setGroup(15)
 basic.forever(function () {
-	
+    radio.sendValue("magnetic", input.magneticForce(Dimension.X))
+    serial.writeValue("magnetic", input.magneticForce(Dimension.X))
 })
