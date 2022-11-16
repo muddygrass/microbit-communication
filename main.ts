9f95a6ca6096d89fd0d435ee4c15c0eb080e4449
muddygrass <118327888+muddygrass@users.noreply.github.com>
2022-11-16 15:04:38 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 radio.onReceivedValue(function (name, value) {
-    serial.writeValue(name, value)
+	
 })
-radio.setGroup(15)
 basic.forever(function () {
-    radio.sendValue("magnetic", input.magneticForce(Dimension.X))
-    serial.writeValue("magnetic", input.magneticForce(Dimension.X))
+	
 })
